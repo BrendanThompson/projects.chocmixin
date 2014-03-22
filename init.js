a2f8ba47c04097663cae73226ca967bb0bb7f84e
Brendan Thompson <brendan@btsystems.com.au>
2014-03-22 14:27:51 +1000

    Initial commit

--- init.js
+++ init.js
@@ -1,23 +1,34 @@
-Hooks.addMenuItem("Projects/New/Choc Mixin", '', function(){
+	// Hooks.addMenuItem("Projects/New/Choc Mixin", '', function(){
+//     //Alert.show("This would be a new project!");
+//     
+//     var mk = require('./lib/files.js');
+//     
+//     mk.createFile("/Users/brendan/", "testme.js");
+// });
+
+// Hooks.addMenuItem("Projects/Test", '', function() {
+//     var win = new Window();
+//     
+//     win.title = "My Window";
+//     win.buttons = ["OK", "Cancel"];
+//     win.onButtonClick = function ("OK") {
+//         Alert.show("clicked");
+//     }
+// });
+
+Hooks.addMenuItem("Projects/New/Test", '', function(){
 	//Alert.show("This would be a new project!");
 	
-	var fs = require('fs');
-	
-	fs.writeFile("/Users/brendan/Development/choc-test/init.js", "utf-8", ,function(err) {
-		if (err) {
-			Alert.show(err);
-		} else {
-			Alert.show("The file was created!");
-		}
-	});
-});
-
-Hooks.addMenuItem("Projects/Test", '', function() {
 	var win = new Window();
-	
-	win.title = "My Window";
-	win.buttons = ["OK", "Cancel"];
-	win.onButtonClick = function ("OK") {
-		Alert.show("clicked");
+	win.htmlPath = "newFile.html";
+	win.buttons = ["OK"];
+	win.onButtonClick = function() {
+		Alert.show(directory + fileName);
+		win.close();
 	}
-})
+	
+	var fileName = String(win.evalExpr('document.getElementById("filename").value'));
+	var directory = String(win.evalExpr('document.getElementById("directory").value'));
+	
+	win.run();
+});
